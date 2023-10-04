403215586ce2e78da27c78123b156d36bf1234bd
Dockins207 <otienodockins48@gmail.com>
2023-10-04 12:42:16 -0700

    python if loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for k in range(0, 100):
     if k == 99:
-        print("{}".format(number))
+        print("{}".format(k))
     else:
-        print("{:02}".format(number), end=", ")
+        print("{:02}".format(k), end=", ")
