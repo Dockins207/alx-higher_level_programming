ec53e14273020ede27819835af0f4a524de430c7
Dockins207 <otienodockins48@gmail.com>
2023-10-04 07:20:53 -0400

    if loops

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-lowercase_alphabets = []
 for i in range(97, 123):
-    lowercase_alphabets(chr(i))
-print(lowercase_alphabets)
+    print(chr(i), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-lowercase_alphabets = []
 for i in range(97, 123):
     if i not in(101, 113):
-        lowercase_alphabets(chr(i))
-print(lowercase_alphabets)
+    print(chr(i), end='')
