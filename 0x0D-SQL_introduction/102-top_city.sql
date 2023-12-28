522404c016374963796dff1f145920c3e749ba6b
Dockins <129792373+Dockins207@users.noreply.github.com>
2023-12-28 15:49:26 +0300

    SQL Introduction

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- listing top 3 citis with highest temperature between the month of july and august
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
